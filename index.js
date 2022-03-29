3be613962dc09777374dfdcc95b1f0affad17232
AwalHossain <waqin21@gmail.com>
2022-03-29 19:35:23 +0600

    response obeject added

--- index.js
+++ index.js
@@ -10,40 +10,23 @@ app.use(cookieParser())
 
 app.set('view engine', 'ejs')
 
-app.locals.title="KOLO"
 
-app.param('id', (req, res, next, id)=>{
-    const user = {
-        id:'5',
-        name:'Bangladesh'
-    }
-
-    req.userDetails = user;
-    next();
-})
 
 app.use(express.static(`${__dirname}/public/`,{
     index: 'homt.html'
 }))
 
 
-app.use('/admin', adminRoute)
-
-adminRoute.get("/dasboard", (req, res)=>{
-    console.log(req.url);
-    console.log(req.baseUrl);
-    console.log(req.originalUrl);
-    res.send("From admin route")
-})
 
 
 app.get("/about/mission/:id",(req, res)=>{
-    console.log(req.url);
-    console.log(req.params);
-    console.log(req.hostname);
-    console.log(req.protocol);
-    console.log(req.originalUrl);
-   res.send('kalm')
+
+    console.log(res.headersSent);
+   res.render('pages/about',{
+       name:"Bangladesh"
+   })
+   console.log(res.headersSent);
+
 })
 
 app.post("/mee",(req, res)=>{
