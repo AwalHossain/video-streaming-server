19f9df99bbf87b1c568f6e0fb71ea81a94bf00ab
Awal hossain <waqin21@gmail.com>
2023-02-19 17:50:11 +0600

    get collection 👍👍

---
+++ server/src/module/models/video/service.js
@@ -0,0 +1,17 @@
+import { getDb } from '../../db/mongo';
+
+
+
+export const collectionName = "videos";
+
+
+export const getCollection = ()=>{
+  const db = getDb();
+  
+  const collection = db.collection(collectionName);
+
+  return collection;
+}
+
+
+
