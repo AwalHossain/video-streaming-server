45930c0d3ddf5ff88fbfe9f096824a9701a967f0
Awal hossain <waqin21@gmail.com>
2023-02-23 15:52:18 +0600

    worker queue has been added �

--- server/src/module/models/queues/worker.ts
+++ server/src/module/models/queues/worker.ts
@@ -1,5 +1,54 @@
-
-
+import { ConnectionOptions, Job, QueueEvents, Worker } from 'bullmq';
+import console from 'console';
+import { QUEUES } from './common';
 const queueName = "video";
 
-// const redisConnection = 
+const redisConnection:ConnectionOptions  = {
+    host: "localhost",
+    port: 6379
+}
+
+export interface JobImp {
+    name: string,
+    payload?: Record<string, unknown>
+    failed:(job: Job) => void
+}
+
+
+Object.values(QUEUES).map((queueName)=>{
+
+    const queueEvents = new QueueEvents(queueName, {
+        connection: redisConnection
+    })
+
+    queueEvents.on("waiting", ({jobId}): void=>{
+        console.log(`A job wih ID ${jobId} is waiting`);
+        
+    })
+
+    queueEvents.on("active", ({jobId, prev, ...others}):void=>{
+        console.log(
+            `Job ${jobId} is now active: previous status was ${prev}`, others
+        ); 
+    })
+    
+    queueEvents.on("failed", ({jobId, failedReason})=>{
+        console.log(`${jobId} has failed with reason ${failedReason}`);
+        
+    })
+
+})
+
+
+export interface WorkerReply {
+    status: number;
+    message: string
+}
+
+const worker = new Worker(
+    queueName,
+    async (job: Job) => {
+      console.log("i am the worker!", job.data);
+    },
+    { connection: redisConnection }
+  );
