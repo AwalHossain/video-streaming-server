56ccf4581715a1a8a8e6b6463939a7c130dcdb16
Awal hossain <waqin21@gmail.com>
2023-02-24 21:47:48 +0600

    code formatted 🎈🎈

--- server/src/module/db/mongo.ts
+++ server/src/module/db/mongo.ts
@@ -1,23 +1,21 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 let db = null;
 
-const connect = async()=>{
-    const client = new MongoClient("mongodb://localhost:27023")
-    console.log("connecting to MongoDB");
-    await client.connect();
-    db = client.db("videodb");
-    console.log("connected to MongoDB");
+const connect = async () => {
+  const client = new MongoClient("mongodb://localhost:27023");
+  console.log("connecting to MongoDB");
+  await client.connect();
+  db = client.db("videodb");
+  console.log("connected to MongoDB");
 
-    return db;
-}
-
+  return db;
+};
 
 // Create a getDb
 
-const getDb = ()=>{
-    return db;
-}
-
+const getDb = () => {
+  return db;
+};
 
 export { connect, getDb };
