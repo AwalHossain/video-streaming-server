79e6e4213ac8d8ece6bc78af493c9b319e05f733
Awal hossain <waqin21@gmail.com>
2023-02-24 21:47:23 +0600

    code formatted 🎈🎈

--- server/src/module/models/video/model.ts
+++ server/src/module/models/video/model.ts
@@ -1,17 +1,11 @@
-import { getDb } from '../../db/mongo';
-
-
+import { getDb } from "../../db/mongo";
 
 export const collectionName = "videos";
 
-
-export const getCollection = ()=>{
+export const getCollection = () => {
   const db = getDb();
-  
+
   const collection = db.collection(collectionName);
 
   return collection;
-}
-
-
-
+};
--- server/src/module/models/video/request.ts
+++ server/src/module/models/video/request.ts
@@ -1,25 +1,19 @@
-
 import Joi from "joi";
 
-
-
 const schema = Joi.object().keys({
-    _id: Joi.string().optional(),
-    title: Joi.string().min(3).max(30).required(),
-    fileName: Joi.string().min(3).max(30).required(),
-    visibility: Joi.string().min(3).max(30).required(),
-    recordingDate: Joi.date().required(),
-    videoLink: Joi.string().min(3).max(30).required()
+  _id: Joi.string().optional(),
+  title: Joi.string().min(3).max(30).required(),
+  fileName: Joi.string().min(3).max(30).required(),
+  visibility: Joi.string().min(3).max(30).required(),
+  recordingDate: Joi.date().required(),
+  videoLink: Joi.string().min(3).max(30).required(),
 });
 
+const validate = (data: object) => {
+  const validationResult = schema.validate(data);
+  console.log(`Validation Result`, validationResult);
 
-
- const validate = (data: object)=>{
-    const validationResult = schema.validate(data);
-    console.log(`Validation Result`, validationResult);
-
-    return validationResult;
-    
-}
+  return validationResult;
+};
 
 export default validate;
