20bd208f7fc0b022fa00f1b797ed3409c8d11611
Awal hossain <waqin21@gmail.com>
2023-02-26 22:02:18 +0600

    video processor added 🤌

---
+++ server/src/module/queues/video-processor.ts
@@ -0,0 +1,44 @@
+import { exec } from "child_process";
+import ffmpeg from "fluent-ffmpeg";
+import path from "path";
+import { promisify } from "util";
+
+const execAsync = promisify(exec);
+
+interface Output {
+  fileName: string;
+  outputFileName: string;
+}
+
+const execute = async (
+  filePath: string,
+  outputFolder: string
+): Promise<Output> => {
+  const fileName = path.basename(filePath);
+  const fileExt = path.extname(filePath);
+
+  const fileNameWithoutExt = path.basename(filePath, fileExt);
+
+  const outputFileName = `${outputFolder}/${fileNameWithoutExt}.mp4`;
+
+  const command = ffmpeg(filePath)
+    .output(outputFileName)
+    .on("start", (commandLine: string) => {
+      console.log(`Spawned Ffmpeg with command`, commandLine);
+    })
+    .on("progress", (progress: ffmpeg.Progress) => {
+      console.log("Processing " + progress.percent + "% done");
+    })
+    .on("end", () => {
+      console.log("Finished processing");
+    })
+    .on("error", (err: Error) => {
+      console.log("An error occurred", err.message);
+    })
+    .run();
+
+  return {
+    fileName,
+    outputFileName,
+  };
+};
