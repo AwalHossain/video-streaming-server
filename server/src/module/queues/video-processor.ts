7832ce9c582f4a268227cdf1bc4eacbb6b6e3514
Awal hossain <waqin21@gmail.com>
2023-02-27 20:02:48 +0600

    hls converting fn added 🎈🎈

--- server/src/module/queues/video-processor.ts
+++ server/src/module/queues/video-processor.ts
@@ -12,7 +12,7 @@ interface Output {
   outputFileName: string;
 }
 
-export const execute = async (
+export const processRawFiletoMp4 = async (
   filePath: string,
   outputFolder: string,
   jobData: object
@@ -36,6 +36,47 @@ export const execute = async (
       console.log("Finished processing");
 
       addQueueItem(QUEUES_EVENTS.VIDEO_PROCESSED, { ...jobData });
+    })
+    .on("error", (err: Error) => {
+      console.log("An error occurred", err.message);
+    })
+    .run();
+
+  return {
+    fileName,
+    outputFileName,
+  };
+};
+
+export const processMp4ToHls = async (
+  filePath: string,
+  outputFolder: string,
+  jobData: object
+): Promise<Output> => {
+  const fileName = path.basename(filePath);
+  const fileExt = path.extname(filePath);
+
+  const fileNameWithoutExt = path.basename(filePath, fileExt);
+  const outputFileName = `${outputFolder}/${fileNameWithoutExt}.m3u8`;
+
+  const command = ffmpeg(filePath)
+    .output(outputFileName)
+    .outputOptions([
+      "-hls_time 10",
+      "-hls_list_size 0",
+      "-hls_segment_filename",
+      `${outputFolder}/${fileNameWithoutExt}_%03d.ts`,
+    ])
+    .on("start", (commandLine: string) => {
+      console.log(`Spawned Ffmpeg with command`, commandLine);
+    })
+    .on("progress", (progress: ffmpeg.Progress) => {
+      console.log("Processing " + progress.percent + "% done");
+    })
+    .on("end", () => {
+      console.log("Finished processing");
+
+      addQueueItem(QUEUES_EVENTS.VIDEO_HLS_CONVERTED, { ...jobData });
     })
     .on("error", (err: Error) => {
       console.log("An error occurred", err.message);
