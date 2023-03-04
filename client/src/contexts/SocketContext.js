135ffb781bf3240fd17c5743b94e565d628bd735
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-03-04 09:06:17 +0000

    socket connection undefined problem solved 🎉🎉

--- client/src/contexts/SocketContext.js
+++ client/src/contexts/SocketContext.js
@@ -1,15 +1,42 @@
-import { createContext, useContext, useMemo } from "react";
-
-import { io } from "socket.io-client";
+import { createContext, useContext, useMemo, useState } from 'react';
+import { io } from 'socket.io-client';
 
 const SocketContext = createContext(null);
 
 export const SocketProvider = ({ children }) => {
-  const socket = useMemo(() => io("http://127.0.0.1:4000"), []);
-  console.log("socket is connec", socket.id);
+  const [socket, setSocket] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useMemo(() => {
+    const newSocket = io('http://127.0.0.1:4000');
+
+    newSocket.on('connect', () => {
+      setSocket(newSocket);
+      setLoading(false);
+    });
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
   );
 };
 
-export const useSocket = () => useContext(SocketContext);
+export const useSocket = () => {
+  const socket = useContext(SocketContext);
+
+  if (!socket) {
+    throw new Error('Socket context not found');
+  }
+
+  return socket;
+};
