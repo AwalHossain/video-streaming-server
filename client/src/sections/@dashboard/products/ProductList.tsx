eb1fac9df110b570231e40ca2fb806a7fdb84d7f
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2023-03-12 14:02:36 +0000

    videoproduct card ✔️

--- client/src/sections/@dashboard/products/ProductList.tsx
+++ client/src/sections/@dashboard/products/ProductList.tsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 // @mui
 import { Grid } from '@mui/material';
+import React from 'react';
 import ShopProductCard from './ProductCard';
 
 // ----------------------------------------------------------------------
@@ -14,7 +15,7 @@ export default function ProductList({ products, ...other }) {
     <Grid container spacing={3} {...other}>
       {products.map((product) => (
         <Grid key={product.id} item xs={12} sm={6} md={3}>
-          <ShopProductCard product={product} />
+          <ShopProductCard video={product} />
         </Grid>
       ))}
     </Grid>
