366c91ced15eb6058ed31ae3523cf41bfc942781
AwalHossain <waqin21@gmail.com>
2023-09-12 12:40:41 +0600

    feat: 🚀🚀Add uploader fn to upload process file into bucket

--- server/src/modules/models/video/video.handler.ts
+++ server/src/modules/models/video/video.handler.ts
@@ -1,50 +1,84 @@
 
-
+import { promises as fsPromises } from "fs";
+import path from "path";
 /// here i am going to update the vidoe history and add the path after each processing
 
+import { S3Client } from "@aws-sdk/client-s3";
 import eventManager from "../../../event-manager";
 import { QUEUE_EVENTS } from "../../queues/constants";
-import { VIDEO_STATUS } from "./video.constant";
 import { VideoService } from "./video.service";
 
-const setupVideoHandler =  () => {
-    Object.values(QUEUE_EVENTS).forEach((queueName) => {
-
-        eventManager.on(queueName, async(data) => {
 
 
-            if(queueName === QUEUE_EVENTS.VIDEO_PROCESSED) {
-                await VideoService.updateHistory(data.id, {
-                    history: {status: 'processed',createdAt: Date.now()},
-                    processedPath: data.path
-                });
-            }
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 
-            if(queueName === QUEUE_EVENTS.VIDEO_THUMBNAIL_GENERATED) {
-                await VideoService.updateHistory(data.id, {
-                    history: {status: 'thumbnail_generated', createdAt: Date.now()},
-                    thumbnailPath: data.path
-                });
-            }
+const setupVideoHandler = () => {
+  const s3 = new S3Client({
+    forcePathStyle: true,
+    endpoint: process.env.ENDPOINT,
+    region: process.env.REGION,
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY,
+      secretAccessKey: process.env.SECRET_KEY,
+    },
+  }) as any;
 
-            if(queueName === QUEUE_EVENTS.VIDEO_HLS_CONVERTED) {
-                await VideoService.updateHistory(data.id, {
-                    history: {status: 'hls_converted', createdAt: Date.now()},
-                    hlsPath: data.path
-                });
-            }
+  Object.values(QUEUE_EVENTS).forEach((queueName) => {
 
-            await VideoService.update(data.id, {
-                status: VIDEO_STATUS.PUBLISHED
-            })
+    eventManager.on(queueName, async (data) => {
 
-            await VideoService.updateHistory(data.id, {
-                history: {status: queueName, createdAt: Date.now()},
+      const uploadProcessedFile = async (folderPath: string, bucketName: string) => {
+        const files = await fsPromises.readdir(folderPath);
+        console.log(files, 'file checking');
+        try {
+
+          for (const file of files) {
+            const filePath = path.join(folderPath, file);
+            const key = file;
+
+            const fileData = await fsPromises.readFile(filePath);
+            const command = new PutObjectCommand({ // Create a PutObjectCommand instance
+              Bucket: bucketName,
+              Key: key,
+              Body: fileData,
+              ACL: "public-read",
             });
 
-            // if(queueName === QUEUE_EVENTS.VIDEO_UPLOAD_PROGRESS) {
+            //   const s3Client = new S3Client({
+            //     region: "sgp1",
+            //   });
+            await s3.send(command);
 
-        })  
+            console.log(`Uploaded: ${key}`);
+          }
+        } catch (error) {
+          console.error("Error uploading folder:", error);
+        }
+
+
+      }
+
+      if (queueName === QUEUE_EVENTS.VIDEO_HLS_CONVERTED) {
+        const rootFolder = path.resolve('./');
+        // destination: 'uploads/videoplayback_1693755779611
+        const file = data.destination.split('/')[1];
+        const folderPath1 = path.join(rootFolder, `./uploads/${file}/hls`);
+        const folderPath2 = path.join(rootFolder, `./uploads/${file}/thumbnails`);
+        console.log("i am the hls converted handler!", data.path, 'checking', folderPath1);
+
+        await uploadProcessedFile(folderPath1, `${file}`);
+        await uploadProcessedFile(folderPath2, `${file}`);
+
+        await VideoService.updateHistory(data.id, {
+          history: { status: queueName, createdAt: Date.now() },
+        });
+      }
+
+
+
+      // if(queueName === QUEUE_EVENTS.VIDEO_UPLOAD_PROGRESS) {
+
     })
+  })
 }
 export default setupVideoHandler;
