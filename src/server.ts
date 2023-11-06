886b9e9ba0ba9586dc6d6b2ac6a1327a738751d9
Awalhossain <waqin21@gmail.com>
2023-11-07 05:03:02 +0600

    Refactor: optimized & ad queue events 📝🔃

--- src/server.ts
+++ src/server.ts
@@ -2,49 +2,28 @@
 import http from "http";
 import { Server } from "socket.io";
 import app from "./app";
-import evenEmitter from "./event-manager";
 
 import mongoose from "mongoose";
-import { NOTIFY_EVENTS } from "./modules/queues/constants";
-import { listenQueueEvent } from "./modules/queues/worker";
+import { setupAllQueueEvent } from "./modules/queues/worker";
 
 const PORT: number = 5000;
 const server = http.createServer(app);
 
 
-const io = new Server(server, {
+export const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
   },
 });
 
-const setup = async () => {
-  // await updateSchema(db);
-  // setupRoutes(app);
+// export const setup = async () => {
+//   // await updateSchema(db);
+//   // setupRoutes(app);
+//   console.log("setup completed.....");
+//   listenQueueEvent(NOTIFY_EVENTS.NOTIFY_VIDEO_HLS_CONVERTED);
 
-  listenQueueEvent(NOTIFY_EVENTS.NOTIFY_VIDEO_HLS_CONVERTED);
-  evenEmitter.on(NOTIFY_EVENTS.NOTIFY_VIDEO_HLS_CONVERTED, (data) => {
-    io.emit("hello", data);
-  });
-
-  evenEmitter.on(NOTIFY_EVENTS.NOTIFY_UPLOAD_PROGRESS, (data) => {
-    io.emit("upload", data);
-  });
-
-  evenEmitter.on(NOTIFY_EVENTS.NOTIFY_VIDEO_UPLOADED, (data) => {
-    io.emit("upload", data);
-  });
-
-  evenEmitter.on(NOTIFY_EVENTS.NOTIFY_VIDEO_METADATA_SAVED, (data) => {
-    io.emit("upload", data);
-  });
-
-  evenEmitter.on(NOTIFY_EVENTS.NOTIFY_VIDEO_PROCESSING, (data) => {
-    io.emit("upload", data);
-  });
-
-};
+// };
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
@@ -54,28 +33,17 @@ io.on("connection", (socket) => {
 
 });
 
-// server.listen(4000, () => {
-//   console.log("listening on *:4000");    P
-// });
-
-
-// Import necessary modules and other code as needed
-
-
 
 server.listen(PORT, async () => {
   console.log(`listening on port ${PORT}`);
   // initializeMongoDB();
 
-  await mongoose.connect(process.env.MONGO_URL)
-  await setup();
-  console.log("application setup completed");
 
-  // app.use("/", (req: Request, res: Response) => {
-  //   console.log(`request received at ${new Date()}`);
-  //   console.log("req", req.body);
-  //   res.send(`request received at ${new Date()}`);
-  // });
+
+  await mongoose.connect(process.env.MONGO_URL)
+  // await setup();
+  setupAllQueueEvent();
+  console.log("application setup completed");
 
   console.log("application started", new Date().toTimeString());
 });
