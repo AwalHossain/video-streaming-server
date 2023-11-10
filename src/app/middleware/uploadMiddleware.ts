b010187410b79d0be8d584fb744a6146b8ba4c57
AwalHo <182204@student.just.edu.bd>
2023-11-10 10:42:19 +0600

    feat: save video meta data when htis server 🚀🚀

--- src/app/middleware/uploadMiddleware.ts
+++ src/app/middleware/uploadMiddleware.ts
@@ -1,6 +1,9 @@
 import { Request } from 'express';
 import fs from "fs";
 import multer from "multer";
+import { VideoService } from '../../modules/models/video/video.service';
+import { NOTIFY_EVENTS } from '../../modules/queues/constants';
+import { io } from '../../server';
 
 
 
@@ -42,9 +45,28 @@ const fileFilter = async (
   if (file.mimetype === "video/mp4" || file.mimetype === "video/x-matroska" || file.mimetype === "video/avi" || file.mimetype === "video/webm"
     || file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'
   ) {
-    console.log("Video file type supported", file);
+    let payload = {
+      originalName: file.originalname,
+      recordingDate: Date.now(),
+      viewCount: 0,
+      duration: 0,
+      visibility: "Public",
+    }
+
+    const videoMetadata = await VideoService.insert(payload);
+    console.log("videoMetadata", videoMetadata);
+    io.emit(NOTIFY_EVENTS.NOTIFY_VIDEO_INITIAL_DB_INFO, {
+      status: "success",
+      message: "Video metadata saved",
+      data: videoMetadata
+    });
+    req.body.videoMetadata = videoMetadata;
     cb(null, true);
   }
+  else if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  }
+
 };
 
 export const uploadHandler = multer({
