2c9ff848073a053a0781066b24e3834465e66d04
AwalHo <182204@student.just.edu.bd>
2023-11-10 19:12:22 +0600

    chore: add env variable for google ☑️

--- src/config/index.ts
+++ src/config/index.ts
@@ -34,6 +34,10 @@ export default {
         port: process.env.REDIS_PORT,
         password: process.env.REDIS_PASSWORD,
         username: process.env.REDIS_USERNAME,
-    }
+    },
+    sessionSecret: process.env.SESSION_SECRET,
+    googleClientId: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    googleCallbackUrl: process.env.GOOGLE_CALLBACK_URL,
 
 }
