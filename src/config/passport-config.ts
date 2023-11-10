b62f50963fb8f67dcfb4bbd1bed5db67e7bfe131
AwalHo <182204@student.just.edu.bd>
2023-11-10 19:02:57 +0600

    feat: passport-config file ⚙️

---
+++ src/config/passport-config.ts
@@ -0,0 +1,36 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import config from '.';
+import { IUser } from '../modules/models/user/user.interface';
+import { User } from '../modules/models/user/user.model';
+
+export const passportConfig = () => {
+    passport.use(new GoogleStrategy({
+        clientID: config.googleClientId,
+        clientSecret: config.googleClientSecret,
+        callbackURL: config.googleCallbackUrl,
+    },
+        async (accessToken, refreshToken, profile, done) => {
+            let user = await User.findOne({ googleId: profile.id });
+
+            if (!user) {
+                user = await User.create({
+                    googleId: profile.id,
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    avatar: profile.photos[0].value,
+                })
+            }
+        }
+    ))
+
+    // serialize user
+    passport.serializeUser((user: IUser, done) => {
+        done(null, user.email);
+    });
+    // deserialize user
+    passport.deserializeUser(async (user, done) => {
+        const userData = await User.findOne({ email: user })
+        done(null, userData);
+    });
+}
