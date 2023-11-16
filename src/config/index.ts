d1e99a3fe740cbd83d04e8d71abe5131f5a72c69
AwalHo <182204@student.just.edu.bd>
2023-11-16 20:05:17 +0600

    Add clientUrl to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -39,5 +39,6 @@ export default {
     googleClientId: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     googleCallbackUrl: process.env.GOOGLE_CALLBACK_URL,
+    clientUrl: process.env.CLIENT_URL,
 
 }
