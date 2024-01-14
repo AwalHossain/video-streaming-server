b434ac04005b9f10ac5a2d481a7679a7b06a3934
Awalhossain <waqin21@gmail.com>
2024-01-14 09:29:21 +0000

    Add domain to session cookie configuration

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,8 @@ app.use(session({
   cookie: {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // true for production
-    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+    domain: process.env.CLIENT_URL
   },
   store: MongoStore.create({
     mongoUrl: process.env.MONGO_URL!,
