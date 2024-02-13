2384faa387b8fbbfbae1238fd51db0f0774f565d
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2024-02-13 10:44:03 +0000

    Update Jest configuration files

--- jest.config.ts
+++ jest.config.ts
@@ -61,10 +61,10 @@ const config: Config = {
   // forceCoverageMatch: [],
 
   // A path to a module which exports an async function that is triggered once before all test suites
-  // globalSetup: undefined,
+  globalSetup: '<rootDir>/test/globalSetup.ts',
 
   // A path to a module which exports an async function that is triggered once after all test suites
-  // globalTeardown: undefined,
+  globalTeardown: '<rootDir>/test/globalTeardown.ts',
 
   // A set of global variables that need to be available in all test environments
   // globals: {},
@@ -137,7 +137,9 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: [
+    '<rootDir>/test/setupFile.ts'
+  ],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
