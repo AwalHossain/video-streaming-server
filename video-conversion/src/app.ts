0add2136e00954a4dc9dcae0b94165059cdde6d0
Awal Hossain <80216813+AwalHossain@users.noreply.github.com>
2024-02-20 16:51:22 +0000

    Add video-conversion app with basic routing

---
+++ video-conversion/src/app.ts
@@ -0,0 +1,35 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express, { Express, NextFunction, Request, Response } from 'express';
+
+// import globalErrorHandler from "./app/middleware/globalErrorhandler";
+
+import router from './app/routes/index';
+const app: Express = express();
+
+dotenv.config();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use(`/api/v1`, router);
+
+// app.use(globalErrorHandler)
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
+
+export default app;
