3838ba085a07efe18a65dbcce5d1fff5370a30f2
awalhossain <waqin21@gmail.com>
2024-02-24 03:42:00 +0600

    Add video event subscriptions and message handling

---
+++ api-server/src/app/modules/video/video.events.ts
@@ -0,0 +1,129 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { redisSubClient } from "../../../shared/redis";
+import { EVENT } from "../../events/event.constants";
+import { VideoService } from "./video.service";
+
+const initVideoEvents = () => {
+  redisSubClient.subscribe(EVENT.INSERT_VIDEO_METADATA_EVENT, (err, count) => {
+    if (err) {
+      console.log("Error in initVideoEvents", err);
+    }
+    console.log("Subscribed to initVideoEvents", count);
+  });
+
+  redisSubClient.subscribe(EVENT.GET_VIDEO_METADATA_EVENT, (err, count) => {
+    if (err) {
+      console.log("Error in initVideoEvents", err);
+    }
+    console.log("Subscribed to initVideoEvents", count);
+  });
+
+  redisSubClient.subscribe(EVENT.VIDEO_PROCESSED_EVENT, (err, count) => {
+    if (err) {
+      console.log("Error in initVideoEvents", err);
+    }
+    console.log("Subscribed to initVideoEvents", count);
+  });
+
+  redisSubClient.subscribe(
+    EVENT.VIDEO_THUMBNAIL_GENERATED_EVENT,
+    (err, count) => {
+      if (err) {
+        console.log("Error in initVideoEvents", err);
+      }
+      console.log("Subscribed to initVideoEvents", count);
+    }
+  );
+
+  redisSubClient.subscribe(EVENT.VIDEO_HLS_CONVERTED_EVENT, (err, count) => {
+    if (err) {
+      console.log("Error in initVideoEvents", err);
+    }
+    console.log("Subscribed to initVideoEvents", count);
+  });
+
+  redisSubClient.subscribe(
+    EVENT.UPLODED_VIDEO_PUBLISHED_EVENT,
+    (err, count) => {
+      if (err) {
+        console.log("Error in initVideoEvents", err);
+      }
+      console.log("Subscribed to initVideoEvents", count);
+    }
+  );
+
+  redisSubClient.subscribe(EVENT.UPDATA_VIDEO_METADATA_EVENT, (err, count) => {
+    if (err) {
+      console.log("Error in initVideoEvents", err);
+    }
+    console.log("Subscribed to initVideoEvents", count);
+  });
+
+  redisSubClient.on("message", async (channel, message) => {
+    if (channel === EVENT.INSERT_VIDEO_METADATA_EVENT) {
+      const data = JSON.parse(message);
+      console.log(
+        `Received the following message from VIDEO_HLS_CONVERTED_EVENT: ${data}`
+      );
+      await VideoService.insertIntoDBFromEvent(data);
+    }
+
+    if (channel === EVENT.VIDEO_PROCESSED_EVENT) {
+      console.log(
+        `Received the following message from VIDEO_HLS_CONVERTED_EVENT: ${message}`
+      );
+      const data = JSON.parse(message);
+      console.log(
+        `Received the following message from VIDEO_HLS_CONVERTED_EVENT: ${data}`
+      );
+      await VideoService.updateHistory(data.id, data.history);
+    }
+
+    if (channel === EVENT.VIDEO_THUMBNAIL_GENERATED_EVENT) {
+      console.log(
+        `Received the following message from VIDEO_HLS_CONVERTED_EVENT: ${message}`
+      );
+      const data = JSON.parse(message);
+      console.log(
+        `Received the following message from VIDEO_HLS_CONVERTED_EVENT: ${data}`
+      );
+      await VideoService.updateHistory(data.id, data.history);
+    }
+
+    if (channel === EVENT.VIDEO_HLS_CONVERTED_EVENT) {
+      console.log(
+        `Received the following message from VIDEO_HLS_CONVERTED_EVENT: ${message}`
+      );
+      const data = JSON.parse(message);
+      console.log(
+        `Received the following message from VIDEO_HLS_CONVERTED_EVENT: ${data}`
+      );
+      await VideoService.updateHistory(data.id, data.history);
+    }
+
+    if (channel === EVENT.UPLODED_VIDEO_PUBLISHED_EVENT) {
+      console.log(
+        `Received the following message from VIDEO_HLS_CONVERTED_EVENT: ${message}`
+      );
+      console.log(
+        `Received the following message from UPLODED_VIDEO_PUBLISHED_EVENT: ${message}`
+      );
+      const data = JSON.parse(message);
+      await VideoService.update(data.id, data.history);
+    }
+
+    // if (channel === EVENT.UPDATA_VIDEO_METADATA_EVENT) {
+    //   console.log(
+    //     `Received the following message from UPDATA_VIDEO_METADATA_EVENT: ${message}`
+    //   );
+    //   const data = JSON.parse(message);
+    //   await VideoService.updateHistory(data.id, {
+    //     history: data.history,
+    //     ...data.rest,
+    //   });
+    // }
+  });
+};
+
+export default initVideoEvents;
