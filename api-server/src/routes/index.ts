58d3776b52cef96c4a1ef5f984aa42c584e72dcb
awalhossain <waqin21@gmail.com>
2024-02-24 03:44:01 +0600

    Refactor routes in index.ts

--- api-server/src/routes/index.ts
+++ api-server/src/routes/index.ts
@@ -1,28 +1,22 @@
-import { Router } from 'express';
-import { UserRoutes } from '../modules/models/user/user.route';
-import { VideoRoutes } from '../modules/models/video/video.route';
-
+import { Router } from "express";
+import { UserRoutes } from "../app/modules/user/user.route";
+import { VideoRoutes } from "../app/modules/video/video.route";
 
 const router = Router();
 
-
-
 const ModuleRoutes = [
-    {
-        path: '/videos',
-        route: VideoRoutes
-    },
-    {
-        path: '/auth',
-        route: UserRoutes
-
-    }
-]
-
+  {
+    path: "/videos",
+    route: VideoRoutes,
+  },
+  {
+    path: "/auth",
+    route: UserRoutes,
+  },
+];
 
 ModuleRoutes.forEach((route) => {
-    router.use(route.path, route.route)
-})
-
+  router.use(route.path, route.route);
+});
 
 export default router;
