4e6cd5aa2527b2b00f554f4149b8ea3af1efa92e
awalhossain <waqin21@gmail.com>
2024-02-24 03:39:28 +0600

    Refactor import path for video events module

--- api-server/src/app/events/index.ts
+++ api-server/src/app/events/index.ts
@@ -1,12 +1,9 @@
-import initVideoEvents from "../../modules/models/video/video.events";
+import initVideoEvents from "../modules/video/video.events";
 
+const subscribeToEvents = () => {
+  console.log("subscribing to events");
 
+  initVideoEvents();
+};
 
-const subscribeToEvents = ()=>{
-    console.log('subscribing to events');
-
-    initVideoEvents();
-    
-}
-    
-    export default subscribeToEvents;
+export default subscribeToEvents;
