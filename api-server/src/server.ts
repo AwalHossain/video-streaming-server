c2991782a4e9717d0bbfaf9df49b5f996e441ec0
awalhossain <waqin21@gmail.com>
2024-02-24 02:53:52 +0600

    Refactor server startup and add database connection

--- api-server/src/server.ts
+++ api-server/src/server.ts
@@ -4,9 +4,11 @@ import { Server } from "socket.io";
 import app from "./app";
 
 import mongoose from "mongoose";
+import subscribeToEvents from "./app/events";
+import config from "./config";
 
 const PORT: number = 5000;
-export const server = http.createServer(app);
+export let server = http.createServer(app);
 
 
 export const io = new Server(server, {
@@ -37,9 +39,24 @@ io.on("connection", (socket) => {
 });
 
 
-server.listen(PORT, async () => {
-  console.log(`listening on port ${PORT}`);
-  await mongoose.connect(process.env.MONGO_URL)
-  console.log("application setup completed successfully");
-  console.log("application started", new Date().toTimeString());
-});
+async function bootstrap() {
+  try{
+
+    subscribeToEvents();
+
+
+    server = app.listen(PORT, async () => {
+      console.log(`listening on port ${PORT}`);
+     const db = await mongoose.connect(config.mongoUrl);
+      console.log('database connected',db.connection.readyState);
+      console.log('application setup completed successfully');
+      console.log('application started', new Date().toTimeString());
+    });
+  } catch (error) {
+    console.error('Error connecting to Redis', error);
+  }
+}
+
+bootstrap();
+
+
