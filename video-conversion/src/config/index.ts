74ea6f386822559b5edf97e0fdfa94676a91e5cd
awalhossain <waqin21@gmail.com>
2024-02-23 23:50:54 +0600

    Add Azure configuration to config file

--- video-conversion/src/config/index.ts
+++ video-conversion/src/config/index.ts
@@ -1,18 +1,22 @@
 import dotenv from 'dotenv';
 
-
 dotenv.config();
 
 const config = {
-    redis:{
-        username: process.env.REDIS_USERNAME,
-        password: process.env.REDIS_PASSWORD,
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
-    },
-    jwt:{
-        secret: process.env.JWT_SECRET
-    }
-}
+  redis: {
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  azure: {
+    accountName: process.env.AZURE_ACCOUNT_NAME,
+    accountKey: process.env.AZURE_ACCOUNT_KEY,
+    containerName: process.env.AZURE_CONTAINER_NAME,
+  },
+};
 
 export default config;
