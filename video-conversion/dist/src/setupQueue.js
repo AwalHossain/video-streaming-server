8433d32ba2923a921ea71b9bd5ddeb3141a22f0e
awalhossain <waqin21@gmail.com>
2024-02-25 02:35:34 +0600

    Delete unused files and code

--- video-conversion/dist/src/setupQueue.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jobWorker_1 = require("./worker/jobWorker");
-const bootstrap = async () => {
-    (0, jobWorker_1.setupAllQueueEvent)();
-    console.log("Queue server has started! port 6379");
-};
-exports.default = bootstrap;
