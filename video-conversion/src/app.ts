b556c170a94ddc73c53d9255538b0e84c79088ec
awalhossain <waqin21@gmail.com>
2024-02-24 14:39:21 +0600

    Add Sentry error monitoring and tracing middleware 💂

--- video-conversion/src/app.ts
+++ video-conversion/src/app.ts
@@ -1,19 +1,48 @@
+import * as Sentry from '@sentry/node';
+import { ProfilingIntegration } from '@sentry/profiling-node';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express, NextFunction, Request, Response } from 'express';
-
 // import globalErrorHandler from "./app/middleware/globalErrorhandler";
-
 import router from './app/routes/index';
-const app: Express = express();
-
+import config from './config';
 dotenv.config();
+
+const app: Express = express();
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+console.log(config.sentry.dsn, 'config.sentry.dsn');
+Sentry.init({
+  dsn: config.sentry.dsn,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+app.get('/debug-sentry', function mainHandler() {
+  throw new Error('My first Sentry error!');
+});
 
 app.use(`/`, router);
+
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 // app.use(globalErrorHandler)
 
