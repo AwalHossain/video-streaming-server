d06a45d4fedf2bfab09bee5feeac420cfd271657
awalhossain <waqin21@gmail.com>
2024-02-27 01:30:54 +0600

    Add RabbitMQ connection and messaging functionality

---
+++ api-server/src/shared/rabbitMQ.ts
@@ -0,0 +1,99 @@
+import client, { Channel, Connection } from "amqplib";
+import { errorLogger, logger } from "./logger";
+
+interface Options {
+  expiration?: string;
+  persistent?: boolean;
+  priority?: number;
+  correlationId?: string;
+  replyTo?: string;
+  // Add more properties as needed
+}
+
+class RabbitMQConnection {
+  connection!: Connection;
+  channel!: Channel;
+  private connected!: boolean;
+
+  async connect(): Promise<void> {
+    if (this.connected && this.channel) return;
+
+    try {
+      logger.info("⏳Connecting to RabbitMQ");
+      this.connection = await client.connect(process.env.RABBITMQ_URL);
+
+      this.connection.on("error", (error) => {
+        errorLogger.error("RabbitMQ connection error", error);
+        // Try to reconnect
+        setTimeout(this.connect, 3000);
+      });
+
+      this.connection.on("close", () => {
+        this.connected = false;
+        logger.info("RabbitMQ connection closed");
+        // Try to reconnect
+        setTimeout(this.connect, 3000);
+      });
+
+      logger.info(`✅ RabbitMQ connected successfully`);
+
+      this.channel = await this.connection.createChannel();
+
+      logger.info(`🛸 RabbitMQ channel created successfully`);
+      this.connected = true;
+    } catch (error) {
+      errorLogger.error("Error connecting to RabbitMQ", error);
+    }
+  }
+
+  // Send message to queue
+  async sendToQueue(queue: string, message: any, options?: Options) {
+    try {
+      if (!this.channel) {
+        await this.connect();
+      }
+
+      this.channel.assertQueue(queue, {
+        durable: false,
+      });
+
+      this.channel.sendToQueue(
+        queue,
+        Buffer.from(JSON.stringify(message)),
+        options
+      );
+    } catch (error) {
+      errorLogger.error("Error sending message to queue", error);
+    }
+  }
+
+  // Consume message from queue
+
+  async consume(
+    queue: string,
+    callback: (message: any, ack: () => void) => void
+  ) {
+    try {
+      if (!this.channel) {
+        await this.connect();
+      }
+
+      this.channel.assertQueue(queue, {
+        durable: false,
+      });
+
+      this.channel.consume(queue, (message) => {
+        if (message) {
+          const ack = () => this.channel.ack(message);
+          callback(message, ack);
+        }
+      });
+    } catch (error) {
+      errorLogger.error("Error consuming message from queue", error);
+    }
+  }
+}
+
+const RabbitMQ = new RabbitMQConnection();
+
+export default RabbitMQ;
