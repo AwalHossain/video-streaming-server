91bff3c5c121caf8c71c7decdf85db6a992f6a46
awalhossain <waqin21@gmail.com>
2024-02-27 01:31:34 +0600

    Refactor isAuthenticated middleware to handle unauthorized requests properly

--- video-conversion/src/app/middleware/isAuthenticated.ts
+++ video-conversion/src/app/middleware/isAuthenticated.ts
@@ -2,30 +2,39 @@
 
 import { NextFunction, Request, Response } from 'express';
 import ApiError from '../../errors/apiError';
-import { errorLogger, logger } from '../../shared/logger';
+import { errorLogger } from '../../shared/logger';
 import { verifyToken } from '../../utils/jwt';
 
 const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
-  logger.info('authHeader', authHeader);
+  try {
+    const authHeader = req.headers.authorization;
 
-  const token = authHeader.split(' ')[1];
-  if (!token) {
-    errorLogger.error('Unauthorized' + ' ' + 'Token not found');
-    throw new ApiError(401, 'Unauthorized');
+    if (!authHeader) {
+      errorLogger.error('Unauthorized' + ' ' + 'No Authorization header');
+      throw new ApiError(401, 'Unauthorized');
+    }
+
+    const token = authHeader.split(' ')[1];
+    console.log('authHeader', token);
+    if (!token) {
+      errorLogger.error('Unauthorized' + ' ' + 'Token not found');
+      throw new ApiError(401, 'Unauthorized');
+    }
+
+    // verify token
+    let verifiedUser = null;
+
+    verifiedUser = verifyToken(token);
+    console.log('verifiedUser', verifiedUser);
+    req.user = verifiedUser; // add user to req object
+
+    console.log('req.user', req.user);
+
+    // use role as authguard
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  // verify token
-  let verifiedUser = null;
-
-  verifiedUser = verifyToken(token);
-
-  req.user = verifiedUser; // add user to req object
-
-  logger.info('req.user', JSON.stringify(req.user));
-
-  // use role as authguard
-  next();
 };
 
 export default isAuthenticated;
