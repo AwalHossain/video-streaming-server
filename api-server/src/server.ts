a2faaeccc5e84b6b1aea19753a96681eb605c021
awalhossain <waqin21@gmail.com>
2024-02-27 01:30:16 +0600

    Add RabbitMQ connection and update server setup

--- api-server/src/server.ts
+++ api-server/src/server.ts
@@ -6,10 +6,10 @@ import app from "./app";
 import mongoose from "mongoose";
 import subscribeToEvents from "./app/events";
 import config from "./config";
+import RabbitMQ from "./shared/rabbitMQ";
 
 const PORT: number = 5000;
 export let server = http.createServer(app);
-
 
 export const io = new Server(server, {
   cors: {
@@ -18,10 +18,8 @@ export const io = new Server(server, {
   },
 });
 
-
 io.on("connection", (socket) => {
   // get the user's id
-
 
   const userId = socket.handshake.query.userId;
 
@@ -31,32 +29,25 @@ io.on("connection", (socket) => {
   // Join the user to the room
   socket.join(userId);
 
-
   socket.on("disconnect", () => {
     console.log(`User ${userId} disconnected`);
   });
-
 });
 
-
 async function bootstrap() {
-  try{
-
+  try {
+    await RabbitMQ.connect();
     subscribeToEvents();
-
-
     server = app.listen(PORT, async () => {
       console.log(`listening on port ${PORT}`);
-     const db = await mongoose.connect(config.mongoUrl);
-      console.log('database connected',db.connection.readyState);
-      console.log('application setup completed successfully');
-      console.log('application started', new Date().toTimeString());
+      const db = await mongoose.connect(config.mongoUrl);
+      console.log("database connected", db.connection.readyState);
+      console.log("application setup completed successfully");
+      console.log("application started", new Date().toTimeString());
     });
   } catch (error) {
-    console.error('Error connecting to Redis', error);
+    console.error("Error connecting to Redis", error);
   }
 }
 
 bootstrap();
-
-
