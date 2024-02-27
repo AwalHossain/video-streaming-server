83f1d772c9d20ff67f8571eaa9a8dfaf107ef5f2
awalhossain <waqin21@gmail.com>
2024-02-27 23:26:36 +0600

    Refactor socket connection in server.ts

--- api-gateway/src/server.ts
+++ api-gateway/src/server.ts
@@ -1,10 +1,9 @@
 import http from 'http';
 import { Server } from 'socket.io';
-import { io as clientIo } from 'socket.io-client';
 import app from './app';
-import { NOTIFY_EVENTS } from './constants/notify';
 import { errorLogger, logger } from './shared/logger';
 import RabbitMQ from './shared/rabbitMQ';
+import { setupRabbitMQConsumers } from './socket';
 
 const PORT: number = 8001;
 
@@ -17,42 +16,21 @@ export const io = new Server(server, {
   },
 });
 
-const videoConversionNamespace = io.of('/video-conversion');
-
-videoConversionNamespace.on('connection', (socket) => {
+io.on('connection', (socket) => {
   const userId = socket.handshake.query.userId;
 
-  // if (!userId) {
-  //   return;
-  // }
   socket.join(userId);
-
-  logger.info(`User ${userId} connected`);
-
-  // Connect to the Video Conversion Server for this user
-  const conversionServerSocket = clientIo(
-    `http://localhost:8000?userId=${userId}`,
-  );
-
-  // Listen for all events from the Video Conversion Server
-  for (const event of Object.values(NOTIFY_EVENTS)) {
-    conversionServerSocket.on(event, (data) => {
-      console.log('event', event, data);
-
-      // Forward the event to the client
-      socket.to(userId).emit(event, data);
-    });
-  }
+  console.log(`User ${userId} connected`);
 
   socket.on('disconnect', () => {
     logger.info(`User ${userId} disconnected`);
-    conversionServerSocket.disconnect();
   });
 });
 
 async function bootstrap() {
   try {
     await RabbitMQ.connect();
+    await setupRabbitMQConsumers();
     server.listen(PORT, async () => {
       logger.info(`listening on port ${PORT}`);
       logger.info('application started');
