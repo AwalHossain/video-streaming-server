53448c5c995815692c8c324f72bf5c0380db0224
awalhossain <waqin21@gmail.com>
2024-02-27 23:29:08 +0600

    Add video processing events

--- video-conversion/src/constant/events.ts
+++ video-conversion/src/constant/events.ts
@@ -12,6 +12,10 @@ export const QUEUE_EVENTS = {
 export const API_SERVER_EVENTS = {
   INSERT_VIDEO_METADATA_EVENT: 'insert-video-metadata',
   GET_VIDEO_METADATA_EVENT: 'get-video-metadata',
+  VIDEO_PROCESSED_EVENT: 'video-processed',
+  VIDEO_HLS_CONVERTED_EVENT: 'video-hls-converted',
+  VIDEO_PUBLISHED_EVENT: 'video-published',
+  VIDEO_THUMBNAIL_GENERATED_EVENT: 'video-thumbnail-generated',
 };
 
 export const API_GATEWAY_EVENTS = {
