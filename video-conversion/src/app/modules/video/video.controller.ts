df36789430081b7c6978735cd91f9bcafd8a560f
awalhossain <waqin21@gmail.com>
2024-02-27 23:28:12 +0600

    Refactor video controller and update event constants

--- video-conversion/src/app/modules/video/video.controller.ts
+++ video-conversion/src/app/modules/video/video.controller.ts
@@ -1,22 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable prefer-const */
 import { Request, Response } from 'express';
-import { NOTIFY_EVENTS, QUEUE_EVENTS } from '../../../constant/queueEvents';
+
+import {
+  API_GATEWAY_EVENTS,
+  API_SERVER_EVENTS,
+  QUEUE_EVENTS,
+} from '../../../constant/events';
 import catchAsync from '../../../errors/catchAsyncError';
-import { getVideoDurationAndResolution } from '../../../handler/videoProcessingHandler';
+import { getVideoDurationAndResolution } from '../../../processor/videoProcessingHandler';
 import { addQueueItem } from '../../../queues/addJobToQueue';
 import { io } from '../../../server';
-import { logger } from '../../../shared/logger';
-import { RedisClient } from '../../../shared/redis';
-import { EVENT } from '../../events/event.constant';
+import RabbitMQ from '../../../shared/rabbitMQ';
 import { videoQueue } from './vidoeQueue';
 
 const uploadVideo = catchAsync(async (req: Request, res: Response) => {
   const userId = req.user.id;
-  logger.info('checking userId', userId);
+  console.log('checking userId', userId);
   {
     if (!req.files['video']) {
-      io.to(userId).emit(NOTIFY_EVENTS.NOTIFY_VIDEO_UPLOADED, {
+      io.to(userId).emit(API_GATEWAY_EVENTS.NOTIFY_VIDEO_UPLOADED, {
         status: 'failed',
         message: 'Video upload is failed',
       });
@@ -25,6 +28,7 @@ const uploadVideo = catchAsync(async (req: Request, res: Response) => {
         .json({ status: 'failed', message: 'Video file is required' });
       return;
     }
+    const video = req.files['video'][0];
 
     // Call the videoQueue function
     const videoMetadata = (await videoQueue(
@@ -34,22 +38,17 @@ const uploadVideo = catchAsync(async (req: Request, res: Response) => {
 
     console.log('videoMetadata checking here', videoMetadata);
 
-    io.to(userId).emit(NOTIFY_EVENTS.NOTIFY_VIDEO_UPLOADED, {
+    // io.to(userId).emit(NOTIFY_EVENTS.NOTIFY_VIDEO_UPLOADED, {
+    //   status: 'success',
+    //   message: 'Video upload is success',
+    // });
+    const uploadMsg = {
+      userId,
       status: 'success',
       message: 'Video upload is success',
-    });
+    };
 
-    // channel.sendToQueue(
-    //   userQueue,
-    //   Buffer.from(
-    //     JSON.stringify({
-    //       status: 'success',
-    //       message: 'Video upload is success',
-    //     }),
-    //   ),
-    // );
-
-    const video = req.files['video'][0];
+    RabbitMQ.sendToQueue(API_GATEWAY_EVENTS.NOTIFY_VIDEO_UPLOADED, uploadMsg);
 
     let image = null;
     if (req.files['image']) {
@@ -71,22 +70,37 @@ const uploadVideo = catchAsync(async (req: Request, res: Response) => {
     //   ...payload,
     // },);
 
-    const sendData = {
+    const updateMetadata = {
       id: videoMetadata._id,
       history: { status: QUEUE_EVENTS.VIDEO_UPLOADED, createdAt: Date.now() },
       ...payload,
     };
 
-    RedisClient.publish(
-      EVENT.UPDATA_VIDEO_METADATA_EVENT,
-      JSON.stringify(sendData),
+    // RedisClient.publish(
+    //   EVENT.UPDATA_VIDEO_METADATA_EVENT,
+    //   JSON.stringify(sendData),
+    // );
+
+    RabbitMQ.sendToQueue(
+      API_SERVER_EVENTS.INSERT_VIDEO_METADATA_EVENT,
+      updateMetadata,
     );
 
-    io.to(userId).emit(NOTIFY_EVENTS.NOTIFY_VIDEO_METADATA_SAVED, {
+    // io.to(userId).emit(API_GATEWAY_EVENTS.NOTIFY_VIDEO_METADATA_SAVED, {
+    //   status: 'success',
+    //   name: 'Video metadata saving',
+    //   message: 'Video metadata saved',
+    // });
+    const metadataMsg = {
+      userId,
       status: 'success',
       name: 'Video metadata saving',
       message: 'Video metadata saved',
-    });
+    };
+    RabbitMQ.sendToQueue(
+      API_GATEWAY_EVENTS.NOTIFY_VIDEO_METADATA_SAVED,
+      metadataMsg,
+    );
 
     await addQueueItem(QUEUE_EVENTS.VIDEO_UPLOADED, {
       userId,
