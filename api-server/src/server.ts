c521ed528536ecd834c9c6a92fa9694913dcb313
awalhossain <waqin21@gmail.com>
2024-02-29 20:07:26 +0600

    Update server port and use logger instead of console

--- api-server/src/server.ts
+++ api-server/src/server.ts
@@ -6,9 +6,10 @@ import app from "./app";
 import mongoose from "mongoose";
 import subscribeToEvents from "./app/events";
 import config from "./config";
+import { errorLogger, logger } from "./shared/logger";
 import RabbitMQ from "./shared/rabbitMQ";
 
-const PORT: number = 5000;
+const PORT: number = 8001;
 export let server = http.createServer(app);
 
 export const io = new Server(server, {
@@ -24,13 +25,13 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
 
   // Log a message
-  console.log(`User ${userId} connected`);
+  logger.info(`User ${userId} connected`);
 
   // Join the user to the room
   socket.join(userId);
 
   socket.on("disconnect", () => {
-    console.log(`User ${userId} disconnected`);
+    logger.info(`User ${userId} disconnected`);
   });
 });
 
@@ -39,14 +40,14 @@ async function bootstrap() {
     await RabbitMQ.connect();
     subscribeToEvents();
     server = app.listen(PORT, async () => {
-      console.log(`listening on port ${PORT}`);
+      logger.info(`listening on port ${PORT}`);
       const db = await mongoose.connect(config.mongoUrl);
-      console.log("database connected", db.connection.readyState);
-      console.log("application setup completed successfully");
-      console.log("application started", new Date().toTimeString());
+      logger.info("database connected", db.connection.readyState);
+      logger.info("application setup completed successfully");
+      logger.info("application started", new Date().toTimeString());
     });
   } catch (error) {
-    console.error("Error connecting to Redis", error);
+    errorLogger.error("Error connecting to Redis", error);
   }
 }
 
