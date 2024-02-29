13fcd7bc00002abb3fb7c1949f60e856d6f7114f
awalhossain <waqin21@gmail.com>
2024-02-29 20:06:46 +0600

    Add JWT utility functions

---
+++ api-gateway/src/utils/jwt.ts
@@ -0,0 +1,17 @@
+import config from '../config';
+
+import jwt from 'jsonwebtoken';
+
+const createToken = (id: string) => {
+  const token = jwt.sign({ id }, config.jwt.secret, {
+    expiresIn: '7d',
+  });
+
+  return token;
+};
+
+const verifyToken = (token: string) => {
+  return jwt.verify(token, config.jwt.secret);
+};
+
+export { createToken, verifyToken };
