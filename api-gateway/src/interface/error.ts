e9146bcd41e55b52e4bcfc501ceb884531539bb0
awalhossain <waqin21@gmail.com>
2024-02-29 20:01:51 +0600

    Add error interface definitions

---
+++ api-gateway/src/interface/error.ts
@@ -0,0 +1,10 @@
+export type IGenericErrorMessage = {
+  path: string | number;
+  message: string;
+};
+
+export type IGenericErrorResponse = {
+  statusCode: number;
+  message: string;
+  errorMessages: IGenericErrorMessage[];
+};
