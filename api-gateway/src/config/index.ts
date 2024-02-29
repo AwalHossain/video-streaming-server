b5b96e1827d278e8d40100755b367f9ba8db3704
awalhossain <waqin21@gmail.com>
2024-02-29 12:06:46 +0600

    Add environment variable configuration for API and video services

--- api-gateway/src/config/index.ts
+++ api-gateway/src/config/index.ts
@@ -1,8 +1,13 @@
 import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config();
+dotenv.config({
+  path: path.join(process.cwd(), '.env'),
+});
 
 const config = {
+  env: process.env.NODE_ENV || 'production',
+  port: process.env.PORT,
   redis: {
     username: process.env.REDIS_USERNAME,
     password: process.env.REDIS_PASSWORD,
@@ -16,9 +21,14 @@ const config = {
     accountName: process.env.AZURE_ACCOUNT_NAME,
     accountKey: process.env.AZURE_ACCOUNT_KEY,
     containerName: process.env.AZURE_CONTAINER_NAME,
+    storage_connection_string: process.env.AZURE_STORAGE_CONNECTION_STRING,
   },
   sentry: {
     dsn: process.env.SENTRY_DSN,
+  },
+  services: {
+    api: process.env.API_SERVICE,
+    video: process.env.VIDEO_SERVICE,
   },
 };
 
