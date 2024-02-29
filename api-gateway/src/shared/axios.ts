b9a600555278661401d065692aa44f04b5a8b5f2
awalhossain <waqin21@gmail.com>
2024-02-29 20:02:49 +0600

    Add HttpService and axios instances 🚀

---
+++ api-gateway/src/shared/axios.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosInstance } from 'axios';
+import config from '../config';
+
+const HttpService = (baseUrl: string): AxiosInstance => {
+  const instance = axios.create({
+    baseURL: baseUrl,
+    timeout: 10000,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  instance.interceptors.request.use(
+    (config) => {
+      return config;
+    },
+    (error) => {
+      return error;
+    },
+  );
+
+  instance.interceptors.response.use(
+    (response) => {
+      return response.data;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+
+  return instance;
+};
+
+const apiService = HttpService(config.services.api);
+const videoService = HttpService(config.services.video);
+
+export { HttpService, apiService, videoService };
