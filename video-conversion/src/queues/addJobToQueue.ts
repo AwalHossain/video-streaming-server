864ca136288db5c4dcb651343536c2b4ecc96e13
awalhossain <waqin21@gmail.com>
2024-02-29 20:24:01 +0600

    Refactor addJobToQueue.ts file

--- video-conversion/src/queues/addJobToQueue.ts
+++ video-conversion/src/queues/addJobToQueue.ts
@@ -1,23 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Queue } from 'bullmq';
-import { ALL_EVENTS as QUEUE_EVENTS } from '../constant/queueEvents';
+import { ALL_EVENTS as QUEUE_EVENTS } from '../constant/events';
 
 import eventEmitter from '../shared/event-manager';
 import { RedisClient } from '../shared/redis';
 
-// const redisConnection = {
-//   username: config.redis.username,
-//   password: config.redis.password,
-//   host: config.redis.host,
-//   port: parseInt(config.redis.port),
-// };
-// const redisConnection = {
-//   host: 'localhost',
-//   port: 6379,
-// };
-
-export type QueueItem = {
-  completed: boolean;
-  path: string;
+type IItem = {
+  [key: string]: any;
 };
 
 type QueueObj = {
@@ -36,7 +25,7 @@ export const queues: QueueObj[] = Object.values(QUEUE_EVENTS).map(
   },
 );
 
-const addQueueItem = async (queueName: string, item: QueueItem) => {
+const addQueueItem = async (queueName: string, item: IItem) => {
   const queue = queues.find((q) => q.name === queueName);
   if (!queue) {
     throw new Error(`queue ${queueName} not found from queues file`);
@@ -50,12 +39,6 @@ const addQueueItem = async (queueName: string, item: QueueItem) => {
     removeOnComplete: true,
     removeOnFail: false,
   });
-
-  //   const newQueue = new Queue(queueName ,{ connection: redisConnection });
-  //  await newQueue.add(queueName, item, {
-  //     removeOnComplete: true,
-  //     removeOnFail: false,
-  //   });
 };
 
 export { addQueueItem };
