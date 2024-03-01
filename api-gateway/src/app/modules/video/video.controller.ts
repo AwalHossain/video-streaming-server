5b796cf503bb204676dc3a38f17bf4532ffcc9e4
awalhossain <waqin21@gmail.com>
2024-03-01 10:03:04 +0600

    Refactor video upload logic and add file renaming

--- api-gateway/src/app/modules/video/video.controller.ts
+++ api-gateway/src/app/modules/video/video.controller.ts
@@ -1,17 +1,20 @@
-import { BlobServiceClient } from '@azure/storage-blob';
 import { Request, Response } from 'express';
-import config from '../../../config';
 import {
   API_SERVER_EVENTS,
   VIDEO_CONVERSION_SERVER,
 } from '../../../constants/event';
 import catchAsync from '../../../shared/catchAsyncError';
-import RabbitMQ from '../../../shared/rabbitMQ';
+import azureUpload from '../../../utils/azureUpload';
+import broadcastVideoEvent from './video.event';
 
 const uploadToBucket = catchAsync(async (req: Request, res: Response) => {
-  console.log('req.file', req.file);
+  // Check if file is present
+  const file = req?.file;
+  // Rename file to remove spaces and add timestamp
+  file.filename =
+    file.originalname.split('.')[0].replace(/\s+/g, '-') + Date.now();
 
-  const file = req.file;
+  const userId = req.user.id;
   if (!file) {
     return res.status(400).json({
       status: 'fail',
@@ -19,63 +22,39 @@ const uploadToBucket = catchAsync(async (req: Request, res: Response) => {
     });
   }
 
-  file.filename =
-    file.originalname.split('.')[0].replace(/\s+/g, '-') + Date.now();
-
-  const blobServiceClient = BlobServiceClient.fromConnectionString(
-    config.azure.storage_connection_string,
-  );
-
+  // azure container name
   const containerName = `container-${new Date().getTime()}`;
-  // Check if container exists
-
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-
-  await containerClient.createIfNotExists({
-    access: 'container',
-  });
-
-  // Create blob client for the specific file location
-  const blockBlobClient = containerClient.getBlockBlobClient(file.filename);
-  // Upload data to the blob
-
-  // Read the file from disk
-  const buffer = file.buffer;
-
-  // Upload data to the blob
-  const uploadBlobResponse = await blockBlobClient.upload(
-    buffer,
-    buffer.length,
-  );
-
-  console.log('uploadBlobResponse', uploadBlobResponse);
+  // call azureUpload function to upload file to azure
+  const response = await azureUpload(file, containerName);
+  console.log('response', response);
 
   const payload = {
     originalName: file.originalname,
     recordingDate: Date.now(),
     duration: '0:00',
     visibility: 'Public',
-    author: '65b7879311d7094f2af84fcb',
+    author: userId,
     fileName: file.filename,
     title: file.originalname.split('.')[0].replace(/[_]/g, ''),
   };
 
-  //   update video Metadata in the database
-  RabbitMQ.sendToQueue(API_SERVER_EVENTS.INSERT_VIDEO_METADATA_EVENT, payload);
+  // broadcast event to api-server to insert video metadata
+  broadcastVideoEvent(API_SERVER_EVENTS.INSERT_VIDEO_METADATA_EVENT, payload);
 
   //   send video-conversion server necessary data
   const data = {
-    userId: '65b7879311d7094f2af84fcb',
-    originalName: file.filename,
+    userId,
+    fileName: file.filename,
     containerName: containerName,
   };
 
-  RabbitMQ.sendToQueue(VIDEO_CONVERSION_SERVER.SEND_VIDEO_METADATA_EVENT, data);
+  // broadcase event to video-conversion server
+  broadcastVideoEvent(VIDEO_CONVERSION_SERVER.SEND_VIDEO_METADATA_EVENT, data);
 
   res.status(200).json({
     status: 'success',
     message: 'File uploaded successfully',
-    data: uploadBlobResponse,
+    response,
   });
 });
 
