e88dd7967b55f8c5b49306bf51b13882617eafc0
awalhossain <waqin21@gmail.com>
2024-03-01 10:05:11 +0600

    Refactor downloadBlob function and import dependencies

--- video-conversion/src/processor/downloadFile.ts
+++ video-conversion/src/processor/downloadFile.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import fs from 'fs';
 import path from 'path';
 import { API_SERVER_EVENTS } from '../constant/events';
-import { blobServiceClient } from '../shared/azure';
+import { IVideoMetadata } from '../interface/common';
 import EventEmitter from '../shared/event-manager';
 import RabbitMQ from '../shared/rabbitMQ';
+import azureDownload from '../utils/azureDownload';
 import initiateVideoProcessing from './initiateVideoProcessing';
 
 const getVideoMetadata = async (): Promise<any> => {
@@ -13,8 +13,8 @@ const getVideoMetadata = async (): Promise<any> => {
     (msg, ack) => {
       const data = JSON.parse(msg.content.toString());
       console.log('vide doe metad', data);
-      ack();
       EventEmitter.emit('videoMetadata', data);
+      ack();
     },
   );
 };
@@ -24,65 +24,34 @@ async function downloadBlob(
   blobName: string,
   userId: string,
 ) {
+  // get video metadata from api-server
   await getVideoMetadata();
 
-  let videoMetadata;
+  let videoMetadata: IVideoMetadata;
   EventEmitter.on('videoMetadata', (data) => {
     videoMetadata = data;
   });
 
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
-  const rootFolder = path.resolve('./');
   let uploadFolder = '';
   if (!uploadFolder) {
     uploadFolder = `container-${new Date().getTime()}`;
   }
-  const rootDidrectory = `${rootFolder}/uploads/${uploadFolder}/videos`;
-  fs.mkdirSync(rootDidrectory, { recursive: true });
 
-  const downloadBlockBlobResponse = await blockBlobClient.download(0);
-
-  const buffer = await streamToBuffer(
-    downloadBlockBlobResponse.readableStreamBody,
-  );
-
-  // write buffer to disk
-  // Assuming `buffer` is your data
-  const downloadPath = path.join(rootDidrectory, blobName);
-  if (buffer instanceof Buffer) {
-    fs.writeFileSync(downloadPath, buffer);
-  } else {
-    console.error('Data is not a Buffer');
-  }
+  // download blob from azure
+  await azureDownload({ containerName, blobName, uploadFolder });
 
   const destination = path
     .normalize(path.join('uploads', uploadFolder, 'videos', blobName))
     .replace(/\\/g, '/');
 
   const videoPath = path.join('uploads', uploadFolder, 'videos', blobName);
-  console.log('Downloaded blob content to:', JSON.stringify(destination));
-
+  console.log('Destination path:', JSON.stringify(destination));
   // intiate video conversion
   await initiateVideoProcessing({
     videoPath,
     destination,
     userId,
     videoMetadata,
-  });
-}
-
-function streamToBuffer(readableStream: NodeJS.ReadableStream) {
-  return new Promise((resolve, reject) => {
-    const chunks = [];
-    readableStream.on('data', (data) => {
-      chunks.push(data instanceof Buffer ? data : Buffer.from(data));
-    });
-    readableStream.on('end', () => {
-      resolve(Buffer.concat(chunks));
-    });
-    readableStream.on('error', reject);
   });
 }
 
