bb6660388c90e668f3dcec23d182fafedc51dce0
awalhossain <waqin21@gmail.com>
2024-03-05 20:22:43 +0600

    Update initiateVideoProcessing.ts with new imports and function call

--- video-conversion/src/processor/initiateVideoProcessing.ts
+++ video-conversion/src/processor/initiateVideoProcessing.ts
@@ -1,9 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // intiaate video Processing
 
-import { API_GATEWAY_EVENTS, QUEUE_EVENTS } from '../constant/events';
+import {
+  API_GATEWAY_EVENTS,
+  API_SERVER_EVENTS,
+  QUEUE_EVENTS,
+} from '../constant/events';
 import { addQueueItem } from '../queues/addJobToQueue';
 import RabbitMQ from '../shared/rabbitMQ';
+import { getVideoDurationAndResolution } from './videoProcessingHandler';
 
 type VideoPath = {
   videoPath: string;
@@ -26,32 +31,23 @@ const initiateVideoProcessing = async ({
     message: 'Video upload is success',
   };
 
-  console.log('uploadMsg', uploadMsg);
-
   //   passing an event to the client to make sure video sucessfully uploaded & downloaded & ready to process
   RabbitMQ.sendToQueue(API_GATEWAY_EVENTS.NOTIFY_VIDEO_UPLOADED, uploadMsg);
 
-  // store download status in the database
-  //   RabbitMQ.sendToQueue(API_SERVER_EVENTS.)
+  const { videoDuration } = await getVideoDurationAndResolution(videoPath);
 
-  // const { videoDuration } = await getVideoDurationAndResolution(video.path);
-
-  // let payload = {
-  //   fileName: video.filename,
-  //   videoPath: video.path,
-  //   watermarkPath: image?.path ?? null,
-  //   title: videoMetadata?.originalName,
-  //   duration: videoDuration,
-  // };
-  const folder = destination.split('/')[1];
   const payload = {
     ...videoMetadata,
+    duration: videoDuration,
     id: videoMetadata._id,
     path: videoPath,
-    folder,
     destination,
   };
-
+  // store download status in the database
+  RabbitMQ.sendToQueue(API_SERVER_EVENTS.UPDATE_METADATA_EVENT, {
+    id: videoMetadata._id,
+    videoPath,
+  });
   await addQueueItem(QUEUE_EVENTS.VIDEO_UPLOADED, {
     userId,
     id: videoMetadata?._id,
