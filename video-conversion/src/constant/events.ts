df65e1383cf04516bd4879b481ca5bc8583b6596
awalhossain <waqin21@gmail.com>
2024-03-05 20:20:07 +0600

    Add new metadata update event

--- video-conversion/src/constant/events.ts
+++ video-conversion/src/constant/events.ts
@@ -16,6 +16,7 @@ export const API_SERVER_EVENTS = {
   VIDEO_HLS_CONVERTED_EVENT: 'video-hls-converted',
   VIDEO_PUBLISHED_EVENT: 'video-published',
   VIDEO_THUMBNAIL_GENERATED_EVENT: 'video-thumbnail-generated',
+  UPDATE_METADATA_EVENT: 'update-metadata',
 };
 
 export const VIDEO_CONVERSION_SERVER = {
@@ -23,6 +24,25 @@ export const VIDEO_CONVERSION_SERVER = {
 };
 
 export const API_GATEWAY_EVENTS = {
+  NOTIFY_VIDEO_HLS_CONVERTED: 'notify.video.hls.converted',
+  NOTIFY_VIDEO_WATERMARKED: 'notify.video.watermarked',
+  NOTIFY_UPLOAD_PROGRESS: 'notify.upload.progress',
+  NOTIFY_VIDEO_UPLOADED: 'notify.video.uploaded',
+  NOTIFY_VIDEO_PROCESSING: 'notify.video.processing',
+  NOTIFY_VIDEO_PROCESSED: 'notify.video.processed',
+  NOTIFY_EVENTS_VIDEO_BIT_RATE_PROCESSED: 'notify.video.bit-rate.processed',
+  NOTIFY_EVENTS_VIDEO_BIT_RATE_PROCESSING: 'notify.video.bit-rate.processing',
+  NOTIFY_VIDEO_THUMBNAIL_GENERATED: 'notify.video.thumbnail-generated',
+  NOTIFY_VIDEO_METADATA_SAVED: 'notify.video.metadata.saved',
+  NOTIFY_EVENTS_FAILED: 'notify.failed',
+  NOTIFY_AWS_S3_UPLOAD_FAILED: 'notify.aws.s3.upload.failed',
+  NOTIFY_AWS_S3_UPLOAD_PROGRESS: 'notify.aws.s3.upload.progress',
+  NOTIFY_AWS_S3_UPLOAD_COMPLETED: 'notify.aws.s3.upload.completed',
+  NOTIFY_VIDEO_PUBLISHED: 'notify.video.published',
+  NOTIFY_VIDEO_INITIAL_DB_INFO: 'notify.video.initial.db.info',
+};
+
+export const NOTIFY_EVENTS = {
   NOTIFY_VIDEO_HLS_CONVERTED: 'notify.video.hls.converted',
   NOTIFY_VIDEO_WATERMARKED: 'notify.video.watermarked',
   NOTIFY_UPLOAD_PROGRESS: 'notify.upload.progress',
