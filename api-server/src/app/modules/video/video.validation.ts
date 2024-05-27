d1cbbaf641312e91ba62fccb37ee073a172d8862
Awalhossain <waqin21@gmail.com>
2024-05-27 14:06:40 +0000

    refactor: Remove visibility field from video validation schema

--- api-server/src/app/modules/video/video.validation.ts
+++ api-server/src/app/modules/video/video.validation.ts
@@ -5,7 +5,6 @@ const schema = Joi.object().keys({
   _id: Joi.string().optional(),
   title: Joi.string().min(3).max(30).required(),
   fileName: Joi.string().min(3).max(30).required(),
-  visibility: Joi.string().min(3).max(30).required(),
   recordingDate: Joi.date().required(),
   videoLink: Joi.string().min(3).max(30).required(),
 });
