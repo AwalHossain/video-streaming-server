707b34a8adcb2735dcc3140ae158d93d713337bf
Awalhossain <waqin21@gmail.com>
2025-04-12 18:21:02 +0600

    feat: Enhance event subscription by adding metadata consumption and improved logging

--- video-conversion/src/app/events/index.ts
+++ video-conversion/src/app/events/index.ts
@@ -1,9 +1,15 @@
 import { logger } from '../../shared/logger';
 import initVideoEvent from '../modules/video/video.events';
+import consumeVideoMetadata from './metadataConsumet';
 
 const subscribeToEvents = () => {
-  logger.info('subscribing to events');
+  logger.info('Subscribing to events...');
+
   initVideoEvent();
+  logger.info('Subscribed to video download trigger events.');
+
+  consumeVideoMetadata();
+  logger.info('Subscribed to metadata consumption events.');
 };
 
 export default subscribeToEvents;
